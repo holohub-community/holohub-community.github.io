d7e628399775ac8d742874ceada7ac67c558f14c
remperazKevin <remperazkevin@gmail.com>
2021-11-11 14:21:17 +0800

    11-11-21 [Major Webpage Additions]

--- functions.js
+++ functions.js
@@ -1,11 +1,29 @@
 function showhome() {
 	document.getElementById('home').style.display = 'flex';
 	document.getElementById('history').style.display = 'none';
+	document.getElementById('affiliation').style.display = 'none';
+	document.getElementById('volunteering').style.display = 'none';
 }
 
 function showHistory() {
 	document.getElementById('home').style.display = 'none';
 	document.getElementById('history').style.display = 'flex';
+	document.getElementById('affiliation').style.display = 'none';
+	document.getElementById('volunteering').style.display = 'none';
+}
+
+function showAffiliation() {
+	document.getElementById('home').style.display = 'none';
+	document.getElementById('history').style.display = 'none';
+	document.getElementById('affiliation').style.display = 'flex';
+	document.getElementById('volunteering').style.display = 'none';
+}
+
+function showVolunteering() {
+	document.getElementById('home').style.display = 'none';
+	document.getElementById('history').style.display = 'none';
+	document.getElementById('affiliation').style.display = 'none';
+	document.getElementById('volunteering').style.display = 'flex';
 }
 
 function opennav() {
